a06b0b02cfe6af255e0578a52c1a7b3ef0662435
Hamed Momeni <hamed.ma7@gmail.com>
2016-08-16 17:22:57 +0430

    I have no idea what I'm doing

---
+++ rxdownloaduploadmanager/src/main/java/com/hmomeni/rxdownloaduploadmanager/TransferObserver.java
@@ -0,0 +1,25 @@
+package com.hmomeni.rxdownloaduploadmanager;
+
+import com.hmomeni.rxdownloaduploadmanager.pojos.Transferable;
+
+import rx.Subscriber;
+
+/**
+ * Created by hamed on 8/16/16.in MyDownloadManager
+ */
+public class TransferObserver extends Subscriber<Transferable> {
+	@Override
+	public void onCompleted() {
+
+	}
+
+	@Override
+	public void onError(Throwable e) {
+
+	}
+
+	@Override
+	public void onNext(Transferable transferable) {
+
+	}
+}
